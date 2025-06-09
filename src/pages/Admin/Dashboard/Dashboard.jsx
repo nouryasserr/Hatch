60b88db7ac6ede50eda31ce262441d1627ce7e35
nouryasserr <nouryasser18113@gmail.com>
2025-06-09 18:47:29 +0300

    commit

--- src/pages/Admin/Dashboard/Dashboard.jsx
+++ src/pages/Admin/Dashboard/Dashboard.jsx
@@ -57,7 +57,7 @@ function Dashboard() {
       <div className="w-full lg:w-5/6 float-end px-8 py-8">
         <div>
           <h1 className="text-2xl xs:text-4xl">dashboard overview</h1>
-          <p className="text-lightgray pt-0.5 text-sm">
+          <p className="text-lightgray text-xs pt-0.5 xs:text-sm">
             monitor key metrics and navigate quickly to manage different
             sections
           </p>
@@ -65,8 +65,8 @@ function Dashboard() {
         <div className="flex flex-col lg:flex-row gap-8 mt-8">
           <div className="lg:w-1/2">
             <div className="mb-4 xs:mb-0">
-              <h2 className="text-3xl mb-0.5">users overview</h2>
-              <p className="text-lightgray text-sm">
+              <h2 className="text-2xl xs:text-3xl mb-0.5">users overview</h2>
+              <p className="text-lightgray text-xs xs:text-sm">
                 overview of all registered users and their status
               </p>
             </div>
@@ -76,12 +76,12 @@ function Dashboard() {
                 <p className="text-sm xs:text-base">blocked users no:</p>
                 <Link
                   to={"/Admin/AllUsers"}
-                  className="block w-fit text-sm text-white bg-primary px-4 py-1.5 rounded-sm border border-lightblack hover:bg-transparent hover:text-black transition duration-300"
+                  className="text-nowrap block w-fit text-sm text-white bg-primary px-4 py-1.5 rounded-sm border border-lightblack hover:bg-transparent hover:text-black transition duration-300"
                 >
                   manage users
                 </Link>
               </div>
-              <div className="w-1/2 space-y-4">
+              <div className="w-1/2 space-y-4 text-end sm:text-start">
                 <p className="text-sm xs:text-base">
                   #{overviewData?.users?.total || 0}
                 </p>
@@ -93,8 +93,10 @@ function Dashboard() {
           </div>
           <div className="lg:w-1/2">
             <div className="mb-4 xs:mb-0">
-              <h2 className="text-3xl mb-0.5">categories overview</h2>
-              <p className="text-lightgray text-sm">
+              <h2 className="text-2xl xs:text-3xl mb-0.5">
+                categories overview
+              </h2>
+              <p className="text-lightgray text-xs xs:text-sm">
                 number of product categories in the system
               </p>
             </div>
@@ -104,16 +106,16 @@ function Dashboard() {
                 <p className="text-sm xs:text-base">subcategories no:</p>
                 <Link
                   to={"/Admin/AllCategories"}
-                  className="block w-fit text-sm text-white bg-primary px-4 py-1.5 rounded-sm border border-lightblack hover:bg-transparent hover:text-black transition duration-300"
+                  className="text-nowrap block w-fit text-sm text-white bg-primary px-4 py-1.5 rounded-sm border border-lightblack hover:bg-transparent hover:text-black transition duration-300"
                 >
                   manage categories
                 </Link>
               </div>
-              <div className="w-1/2 space-y-4">
+              <div className="w-1/2 space-y-4 text-end sm:text-start">
                 <p className="text-sm xs:text-base">
                   #{overviewData?.categories?.total || 0}
                 </p>
-                <p className="text-nowrap text-xs xs:text-base">
+                <p className="text-xs xs:text-base">
                   #{overviewData?.categories?.sub_categories || 0}
                 </p>
               </div>
@@ -123,24 +125,30 @@ function Dashboard() {
         <div className="flex flex-col lg:flex-row gap-8 mt-8 lg:mt-16">
           <div className="lg:w-1/2">
             <div className="mb-4 xs:mb-0">
-              <h2 className="text-3xl mb-0.5">startups overview</h2>
-              <p className="text-lightgray text-sm">
+              <h2 className="text-2xl xs:text-3xl mb-0.5">startups overview</h2>
+              <p className="text-lightgray text-xs xs:text-sm">
                 overview of all registered startups and their status
               </p>
             </div>
             <div className="flex gap-2 mt-4">
               <div className="w-1/2 space-y-4">
-                <p className="text-sm xs:text-base">active startups no:</p>
-                <p className="text-sm xs:text-base">pending approvals no:</p>
-                <p className="text-sm xs:text-base">blocked startups no:</p>
+                <p className="text-nowrap text-sm xs:text-base">
+                  active startups no:
+                </p>
+                <p className="text-nowrap text-sm xs:text-base">
+                  pending approvals no:
+                </p>
+                <p className="text-nowrap text-sm xs:text-base">
+                  blocked startups no:
+                </p>
                 <Link
                   to={"/Admin/AllStartups"}
-                  className="block w-fit text-sm text-white bg-primary px-4 py-1.5 rounded-sm border border-lightblack hover:bg-transparent hover:text-black transition duration-300"
+                  className="text-nowrap block w-fit text-sm text-white bg-primary px-4 py-1.5 rounded-sm border border-lightblack hover:bg-transparent hover:text-black transition duration-300"
                 >
                   manage startups
                 </Link>
               </div>
-              <div className="w-1/2 space-y-4">
+              <div className="w-1/2 space-y-4 text-end sm:text-start">
                 <p className="text-sm xs:text-base">
                   #{overviewData?.startups?.active || 0}
                 </p>
@@ -155,24 +163,30 @@ function Dashboard() {
           </div>
           <div className="lg:w-1/2">
             <div className="mb-4 xs:mb-0">
-              <h2 className="text-3xl mb-0.5">factories overview</h2>
-              <p className="text-lightgray text-sm">
+              <h2 className="text-2xl xs:text-3xl mb-0.5">
+                factories overview
+              </h2>
+              <p className="text-lightgray text-xs xs:text-sm">
                 number of production factories and activity level
               </p>
             </div>
             <div className="flex gap-2 mt-4">
               <div className="w-1/2 space-y-4">
                 <p className="text-sm xs:text-base">factories no:</p>
-                <p className="text-sm xs:text-base">active factories no:</p>
-                <p className="text-sm xs:text-base">blocked factories no:</p>
+                <p className="text-nowrap text-sm xs:text-base">
+                  active factories no:
+                </p>
+                <p className="text-nowrap text-sm xs:text-base">
+                  blocked factories no:
+                </p>
                 <Link
                   to={"/Admin/AllFactories"}
-                  className="block w-fit text-sm text-white bg-primary px-4 py-1.5 rounded-sm border border-lightblack hover:bg-transparent hover:text-black transition duration-300"
+                  className="text-nowrap block w-fit text-sm text-white bg-primary px-4 py-1.5 rounded-sm border border-lightblack hover:bg-transparent hover:text-black transition duration-300"
                 >
                   manage factories
                 </Link>
               </div>
-              <div className="w-1/2 space-y-4">
+              <div className="w-1/2 space-y-4 text-end sm:text-start">
                 <p className="text-sm xs:text-base">
                   #{overviewData?.factories?.total || 0}
                 </p>
